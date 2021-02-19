a56eb5cf1b0f9caafccaeef3c720d905cf3f91ff
Joana Trashlieva <joana@trashlieva.com>
2021-02-19 00:23:33 +0000

    style of carousel

--- assets/js/json.js
+++ assets/js/json.js
@@ -1,11 +1,32 @@
 $.getJSON("assets/results.json", function(json) {
-      for (var o = 0; o < json.length; o++) {
-        $('.instagram-carousel').append('<img src=' + json[o].imgUrl + '></>');
+  for (var o = 0; o < json.length; o++) {
+    $('.instagram-carousel').append('<img src=' + json[o].imgUrl + '></>');
+  }
+
+  $('.instagram-carousel').slick({
+    centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
       }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
 });
 
-$('.instagram-carousel').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
